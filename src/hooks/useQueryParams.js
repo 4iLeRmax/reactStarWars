90bc6dbd10a34b128ffcbd60d4bb6f7f7cdd989d
4iLeRmax <max.zlatin@gmail.com>
2023-04-10 17:22:51 +0300

    files

--- src/hooks/useQueryParams.js
+++
@@ -1,6 +0,0 @@
-import { useLocation } from "react-router"
-
-export const useQueryParams = (param)=>{
-  // return new URLSearchParams(useLocation().search);
-  return useLocation().search.replace(`?${param}=`, '');
-}
--- src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import { favouriteReducer } from "./favouriteReducer";
-
-export const rootReducer = combineReducers({
-  favouriteReducer
-});
